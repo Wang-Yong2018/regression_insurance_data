4c4621b077eb1b004b1b2ca54e4b550a01da6622
wang.yong <w.yong@yahoo.com>
2024-12-12 17:44:30 +0800

    update etl.R and demo.rmd 1. add v0.8 no big improvement 2. eda by tree , still no found the 20usd clue

--- etl.R
+++ etl.R
@@ -4,7 +4,7 @@ library(duckdb)
 library(dbplyr)
 library(purrr)
 library(vetiver)
-
+library(lubridate)
 
 init_db <- function(do=FALSE) {
   if (do==TRUE) {
@@ -17,7 +17,9 @@ init_db <- function(do=FALSE) {
     con <- dbConnect(drv)
     
     file_names |> iwalk(\(file_name, table_name) duckdb::duckdb_read_csv(con,table_name, file_name)) 
+    
     dbDisconnect(con)
+    
     print('transform csv files into duckdb')
   }
   
@@ -29,36 +31,51 @@ get_duck_con <- function() {
   duck_dir <- './input/insurance.duckdb'
   drv <- duckdb(duck_dir,read_only=TRUE)
   con <- dbConnect(drv)
-  
+  return(con)
 }
 
 get_train <- function(){
-  file_name <- './input/train.csv'
   con <- get_duck_con()
   ddb <-
-    dbReadTable(con,'train')|> 
-    mutate(`Policy.Start.Date`=ymd_hms(`Policy.Start.Date`))
+    tbl(con,'train') |>
+    mutate(Policy.Start.Date = as_datetime(Policy.Start.Date))
+    #dbReadTable(con,'train_t')|> 
   
-  dbDisconnect(con)
+  #dbDisconnect(con)
   return(ddb)
+}
+
+get_test <- function(){
+  con <- get_duck_con()
+  ddb <- 
+    tbl(con,'test')|>
+    mutate(Policy.Start.Date=as_datetime(Policy.Start.Date))
+  
+   # dbReadTable(con,'test') |>
+  #dbDisconnect(con)
+  return(ddb)
+  
 }
 
 get_sample <- function(){
   file_name <- './input/sample_submission.csv'
   con <- get_duck_con()
-  ddb <- dbReadTable(con,'sample')
-  
-  dbDisconnect(con)
+  ddb <- 
+    tbl(con,'sample')
+   # dbReadTable(con,'sample')
+  #dbDisconnect(con)
   return(ddb)
 }
 
-get_test <- function(){
-  file_name <- './input/test.csv'
-  con <- get_duck_con()
-  ddb <- 
-    dbReadTable(con,'test') |>
-    mutate(`Policy.Start.Date`=ymd_hms(`Policy.Start.Date`))
-  dbDisconnect(con)
-  return(ddb)
+get_colname_by_type <- function(type='numeric'){
+  sample_df <- get_train() |> head(100) |>collect()
   
+  sample_df_filted <-
+    switch(type,
+           'numeric'=sample_df|> select(where(is.numeric)),
+           'character'=sample_df|> select(where(is.character)),
+           'datetime'=sample_df|> select(where(is.POSIXt))
+           )
+  colnames <- sample_df_filted |> colnames()
+  return(colnames)
 }
