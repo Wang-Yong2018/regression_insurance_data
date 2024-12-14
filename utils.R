eb4dec00672097fa49197172e431d0eb9aabcbb3
wangyong <wangyong_cn@foxmail.com>
2024-12-14 22:57:34 +0800

    v1.02 done. rmsel reach 1.02 and rsq reach 10%. but huge missing value only 3.8 *10**5 rows available.

--- utils.R
+++ utils.R
@@ -33,10 +33,10 @@ get_fit_wf <- function(rcp,data,name='',description='', is_log1py=FALSE,return_w
   fit_wf <- lm_wf |>
     add_recipe(rcp)|>
     fit({{data}}) 
-  
   fit_mod <- 
     fit_wf |> 
     extract_fit_engine()
+  #tmp_baked <- rcp|>prep()|>check_missing(all_predictors())|> bake(data) 
   
   tidy_mod <- fit_mod |> broom::tidy()
   glance_mod <- fit_mod |> broom::glance()
@@ -104,3 +104,80 @@ get_fit_wf <- function(rcp,data,name='',description='', is_log1py=FALSE,return_w
 }
 
 
+get_cut_pct <- function(df) {
+  
+  income_pct <- df |> pull(Annual.Income) |> quantile(
+    x = _,
+    probs = seq(0, 1, 0.05),
+    na.rm = TRUE,
+    include.lowest = T
+  )
+  age_pct <- df|> pull(Age) |> quantile(
+    x = _,
+    probs = seq(0, 1, 0.1),
+    na.rm = TRUE,
+    include.lowest = T
+  )
+  date_pct <- df|>pull(Policy.Start.Date)|> quantile(
+    x=_, 
+    probs=seq(0,1,0.05),
+    na.rm = TRUE,
+    include.lowest = T
+  )
+  Credit_pct <-df |>pull(Credit.Score)|> quantile(
+    x=_, 
+    probs=seq(0,1,0.1),
+    na.rm = TRUE,
+    include.lowest = T
+  )
+  Health_pct <- df|>pull(Health.Score)|> quantile(
+    x=_, 
+    probs=seq(0,1,0.1),
+    na.rm = TRUE,
+    include.lowest = T
+  )
+ 
+  result<-list('income_pct'=income_pct,
+               'age_pct'=age_pct,
+               'date_pct'=date_pct,
+               'Credit_pct'=Credit_pct,
+               'Health_pct'=Health_pct)
+ return(result) 
+}
+
+get_grp_feature<- function(df){
+  grp_col <-c('Gender','Marital.Status',
+              'Number.of.Dependents', 'Education.Level',
+              'Occupation','Location', 'Policy.Type',
+              'Smoking.Status',
+              'Exercise.Frequency',
+              'Property.Type' )
+  result_pct <- df|>get_cut_pct() 
+  tmp_df <- df|>
+    collect()|>
+    mutate( Annual.Income_cut = cut(Annual.Income,  breaks = result_pct$income_pct, include.lowest=TRUE),
+            Credit.Score_cut = cut(Credit.Score,  breaks = result_pct$Credit_pct,  include.lowest=TRUE),
+            Health.Score_cut = cut(Health.Score,  breaks = result_pct$Health_pct,  include.lowest=TRUE),
+            Age_cut = cut(Age,  breaks = result_pct$age_pct,  include.lowest=TRUE)
+            
+    )
+  grp_df <-
+    tmp_df |>
+    group_by(across(all_of(grp_col)),.drop=TRUE ) |>
+    summarise(
+      cnt=n(),
+      mean_income=mean(Annual.Income,na.rm=TRUE),
+      mean_age=mean(Age,na.rm=TRUE),
+      min_premium = mean(Premium.Amount),
+      max_premium = max(Premium.Amount),
+      sd_premium =  coalesce(sd(Premium.Amount),0),
+      ids=list(id),
+      .groups = 'drop') |>
+    ungroup()|>
+    select(-c(grp_col))|>
+    unnest_longer(col=ids,values_to='id',indices_to='id_seq')|>
+    arrange(id)
+  
+  
+  return(grp_df)
+}
